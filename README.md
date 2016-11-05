2284ffedba285efbc109acac040cba1369fa545e
yasharma <yashsharma205@gmail.com>
2016-11-05 12:14:04 +0530

    Understanding isolate scopes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # understanding-angular-directives
-Angular Directives
+
+### `'&'` is used to invoke an expression, call an expression and function
+### communicate between controller and directive.
+
+### `'='` is used to assign value to directivE
--- js/app.js
+++ js/app.js
@@ -3,17 +3,29 @@
 angular.module('myApp', [])
 	.controller('choreCtrl', function($scope){
 		$scope.logChore = function(task){
-			console.log(task, ' is done');
+			alert(task + ' is done');
 		};
+	})
+	.controller('drinkCtrl', function($scope){
+		$scope.ctrlFlavor = 'Fzz';
 	})
 	.directive('kid', function(){
 		return {
 			restrict: 'E',
 			scope: {
-				done: '&' // & is used to call function and for expressions
+				done: '&' // & is used to call function and to invoke an expressions
 			},
 			template: '<input type="text" ng-model="chore">'+
-				'{{chore}}'+
+				'{{chore}}<br>'+
 				'<button ng-click="done({task:chore})">Done</button>'
+		}
+	})
+	.directive('drink', function(){
+		return {
+			restrict: 'A',
+			scope: {
+				flavor: '=' // & is used to call function and to invoke an expressions
+			},
+			template: '<input type="text" ng-model="flavor">'
 		}
 	});
